1795a374fbe43e99a4cf053fc5bb3f6df19294dc
t-akzw <45358756+t-akzw@users.noreply.github.com>
2020-03-20 18:12:12 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,28 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-57df575434dfba5b6f7c.js"
+    "url": "webpack-runtime-5d9450a6cdf2b6f7cd63.js"
   },
   {
     "url": "commons-17a974655e02519dfbff.js"
   },
   {
-    "url": "app-b00a11a48b108bf7e34b.js"
+    "url": "app-f1da6990e45548e37733.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-7ca1fffc0f095fe8aa22.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d4dd08dd3d0b906e5230b8dd5904c0b3"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "c355c8040c47a63bfb3360e4b7cb6553"
-  },
-  {
-    "url": "page-data/app-data.json",
-    "revision": "a5d089c23d687008a33d72a17122a272"
+    "revision": "750fbe483595c51363faa5b7c9fa6a06"
   },
   {
     "url": "manifest.webmanifest",
@@ -70,12 +62,12 @@ const { NavigationRoute } = workbox.routing
 
 const navigationRoute = new NavigationRoute(async ({ event }) => {
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/blog`), ``)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/blog/app-b00a11a48b108bf7e34b.js`))) {
+  if (!resources || !(await caches.match(`/app-f1da6990e45548e37733.js`))) {
     return await fetch(event.request)
   }
 
@@ -88,7 +80,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/blog/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
   return await caches.match(offlineShell)
 })
 
